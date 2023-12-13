a90176351e7506405032196c9f12055692cddca9
Aswin K S <k.aswin@entri.me>
2023-12-13 17:54:36 +0530

    Entri 1.2.16.0

--- room-kit/src/main/java/live/hms/roomkit/ui/meeting/MeetingFragment.kt
+++ room-kit/src/main/java/live/hms/roomkit/ui/meeting/MeetingFragment.kt
@@ -428,7 +428,14 @@ class MeetingFragment : Fragment() {
         requireActivity().finish()
     }
 
-    private fun updateRecordingViews(state: HMSRecordingState) {
+    private fun updateRecordingViews(state: HMSRecordingState, shouldShowRecordingIcons : Boolean?) {
+        if (shouldShowRecordingIcons == false){
+            binding.recordingSignalProgress.visibility = View.GONE
+            binding.recordingSignal.visibility = View.GONE
+            binding.recordingPause.visibility = View.GONE
+            return
+        }
+
         when (state) {
             HMSRecordingState.STARTING -> {
                 binding.recordingSignalProgress.visibility = View.VISIBLE
@@ -464,7 +471,10 @@ class MeetingFragment : Fragment() {
 
             HMSStreamingState.STARTED -> {
                 binding.meetingFragmentProgress.visibility = View.GONE
-                binding.liveTitleCard.visibility = View.VISIBLE
+
+                /** binding.liveTitleCard.visibility = View.VISIBLE **/
+                binding.liveTitleCard.visibility = if (meetingViewModel.isLiveIconEnabled==false) View.GONE else View.VISIBLE
+
                 if (meetingViewModel.isRTMPRunning()) {
                     binding.liveTitle.text = "Live with RTMP"
                 } else {
@@ -530,7 +540,7 @@ class MeetingFragment : Fragment() {
         }
 
         meetingViewModel.recordingState.observe(viewLifecycleOwner) { state ->
-            updateRecordingViews(state)
+            updateRecordingViews(state , meetingViewModel.isRecordingIconsEnabled)
         }
 
         meetingViewModel.streamingState.observe(viewLifecycleOwner) { state ->
@@ -1377,16 +1387,30 @@ class MeetingFragment : Fragment() {
             if (it.isEnabled) meetingViewModel.flipCamera()
         }
 
-        if (meetingViewModel.getHmsRoomLayout()?.data?.getOrNull(0)?.logo?.url.isNullOrEmpty()) {
-            binding.logoIv?.visibility = View.GONE
-        } else {
+        if (!meetingViewModel.roomLogoUrl.isNullOrEmpty()){
             binding.logoIv.visibility = View.VISIBLE
             binding.logoIv.let {
                 Glide.with(this)
-                    .load(meetingViewModel.getHmsRoomLayout()?.data?.getOrNull(0)?.logo?.url)
+                    .load(meetingViewModel.roomLogoUrl)
                     .into(it)
             }
+        }else{
+            binding.logoIv.visibility = View.GONE
         }
+
+        /***
+        if (meetingViewModel.getHmsRoomLayout()?.data?.getOrNull(0)?.logo?.url.isNullOrEmpty()) {
+        binding.logoIv?.visibility = View.GONE
+        } else {
+        binding.logoIv.visibility = View.VISIBLE
+        binding.logoIv.let {
+        Glide.with(this)
+        .load(meetingViewModel.getHmsRoomLayout()?.data?.getOrNull(0)?.logo?.url)
+        .into(it)
+        }
+        }
+         ***/
+
     }
 
     private fun isOverlayChatVisible() : Boolean {
